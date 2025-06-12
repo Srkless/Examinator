0fa383bfbe789c6fc11f677c41b2127ef49c6daa
Marko Maksimovic <kmeekmeekmee@gmail.com>
2025-06-12 16:23:58 +0200

    Finished parser.

--- examinator-server/src/main/java/net/etfbl/examinator/parsers/FormulaParser.java
+++ examinator-server/src/main/java/net/etfbl/examinator/parsers/FormulaParser.java
@@ -1,10 +1,40 @@
 package net.etfbl.examinator.parsers;
 
-import net.etfbl.examinator.models.StudentSubject;
+import com.googlecode.aviator.AviatorEvaluator;
+import lombok.*;
+import java.util.*;
 
-import java.util.List;
-import java.util.function.Predicate;
-
+/**
+ * <H1>
+ * Simple Formula parsing class. It relies on Google Aviator Expression Language.
+ * </H1>
+ *
+ * @author Marko Maksimovic
+ * @version 1.0
+ * For further information about the parsing engine, refer to: <a href="https://mvnrepository.com/artifact/com.googlecode.aviator/aviator/5.4.3">...</a>
+ */
+@Getter
+@Setter
 public class FormulaParser implements Parser {
+    private String formula;
+
+    /**
+     * Constructs a FormulaParser object to parse the given formula.
+     * @param formula Formula to parse.
+     */
+    public FormulaParser(String formula) {
+        this.formula = formula;
+    }
+
+    /**
+     *
+     * @param studentResults map containing student results mapped to activity identifiers.
+     * @return number of points a student has achieved.
+     */
+    public int evaluate(Map<String, Integer> studentResults) {
+        Map<String, Object> env = new HashMap<>(studentResults);
+
+        return Integer.parseInt(AviatorEvaluator.execute(formula, env).toString());
+    }
 
 }
